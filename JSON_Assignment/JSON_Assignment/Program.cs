7d46a745c28e3953dc9379d61c6b5dfcf04431ba
blake.swier <blake.swier@mindbodyonline.com>
2013-03-18 16:52:11 -0700

    Json assignment work

--- JSON_Assignment/JSON_Assignment/Program.cs
+++ JSON_Assignment/JSON_Assignment/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace JSON_Assignment
 {
@@ -10,6 +12,17 @@ namespace JSON_Assignment
     {
         static void Main(string[] args)
         {
+            string myData = RedditData.GetData();
+            //string[] words = Regex.Split(myData, ":,");
+            //foreach (var s in words)
+            //{
+            //    Console.WriteLine(words);
+            //}
+            
+            Console.WriteLine(myData);
+            Console.ReadLine();
         }
+
+      
     }
 }
---
+++ JSON_Assignment/JSON_Assignment/RedditData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_Assignment
+{
+    public class RedditData
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public int Score { get; set; }
+        public string Permalink { get; set; }
+        public string Author { get; set; }
+
+        public static string GetData()
+        {
+            var stream = File.OpenText("redditfront.txt");
+            string text = stream.ReadToEnd();
+
+            //StreamReader myReader = new StreamReader("redditfront.json");
+            //string line = "";
+            //string result = "";
+
+            //while (line != null)
+            //{
+            //    line = myReader.ReadLine();
+            //    if (line != null)
+            //    {
+            //        result += line;
+            //    }
+            //}
+            //myReader.Close();
+
+            return text;
+
+        }
+    }
+
+   
+}
